75ce0552a7f0b90acfd559d1d7ea7405cae0bce3
cubuspl42 <cubuspl42@gmail.com>
2022-10-28 18:53:55 +0200

    Nuke unused `Table` subclasses

--- src/main/kotlin/sigma/values/tables/EmptyTable.kt
+++
@@ -1,9 +0,0 @@
-package sigma.values.tables
-
-import sigma.values.Value
-
-object EmptyTable : Table() {
-    override fun read(argument: Value): Value? = null
-
-    override fun dumpContent(): String? = null
-}
--- src/main/kotlin/sigma/values/tables/Scope.kt
+++ src/main/kotlin/sigma/values/tables/Scope.kt
@@ -5,12 +5,6 @@ import sigma.values.Symbol
 import sigma.values.Value
 
 abstract class Scope : Table() {
-    object Empty : Scope() {
-        override fun get(name: Symbol): Thunk? = null
-
-        override fun dumpContent(): String? = null
-    }
-
     final override fun read(argument: Value): Thunk? {
         if (argument !is Symbol) return null
 
