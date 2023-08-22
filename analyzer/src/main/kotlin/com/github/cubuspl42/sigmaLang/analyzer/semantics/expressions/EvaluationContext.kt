edf0456114332940c9bbf02d9a24bd627782748c
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-08-22 19:09:00 +0200

    Extract some code from `Expression.kt`

---
+++ analyzer/src/main/kotlin/com/github/cubuspl42/sigmaLang/analyzer/semantics/expressions/EvaluationContext.kt
@@ -0,0 +1,17 @@
+package com.github.cubuspl42.sigmaLang.analyzer.semantics.expressions
+
+data class EvaluationContext(
+    val evaluationDepth: Int,
+) {
+    companion object {
+        val Initial: EvaluationContext = EvaluationContext(
+            evaluationDepth = 0,
+        )
+
+        const val maxEvaluationDepth: Int = 2048
+    }
+
+    fun withIncreasedDepth(): EvaluationContext = EvaluationContext(
+        evaluationDepth = evaluationDepth + 1,
+    )
+}
