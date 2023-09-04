2ea44acd763b498c74bbf4aa2288bb81145df3b3
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-04 10:53:24 +0200

    Nuke `Declaration`

--- analyzer/src/main/kotlin/com/github/cubuspl42/sigmaLang/analyzer/semantics/ValueDeclaration.kt
+++ analyzer/src/main/kotlin/com/github/cubuspl42/sigmaLang/analyzer/semantics/ValueDeclaration.kt
@@ -1,8 +1,11 @@
 package com.github.cubuspl42.sigmaLang.analyzer.semantics
 
+import com.github.cubuspl42.sigmaLang.analyzer.evaluation.values.Symbol
 import com.github.cubuspl42.sigmaLang.analyzer.evaluation.values.Thunk
 import com.github.cubuspl42.sigmaLang.analyzer.semantics.types.Type
 
-interface ValueDeclaration : Declaration {
+interface ValueDeclaration {
+    val name: Symbol
+
     val effectiveValueType: Thunk<Type>
 }
