0c03a868a8cc47767483c17dfb5eb3aabc2ce85d
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-10-12 13:53:13 +0200

    Move `TranslationDynamicScope` out of `expressions` package

--- analyzer/src/main/kotlin/com/github/cubuspl42/sigmaLang/analyzer/semantics/TranslationDynamicScope.kt
+++ analyzer/src/main/kotlin/com/github/cubuspl42/sigmaLang/analyzer/semantics/TranslationDynamicScope.kt
@@ -1,11 +1,9 @@
-package com.github.cubuspl42.sigmaLang.analyzer.semantics.expressions
+package com.github.cubuspl42.sigmaLang.analyzer.semantics
 
 import com.github.cubuspl42.sigmaLang.analyzer.evaluation.scope.DynamicScope
-import com.github.cubuspl42.sigmaLang.analyzer.evaluation.values.Identifier
 import com.github.cubuspl42.sigmaLang.analyzer.evaluation.values.Symbol
 import com.github.cubuspl42.sigmaLang.analyzer.evaluation.values.Thunk
 import com.github.cubuspl42.sigmaLang.analyzer.evaluation.values.Value
-import com.github.cubuspl42.sigmaLang.analyzer.semantics.StaticScope
 import com.github.cubuspl42.sigmaLang.analyzer.semantics.introductions.ConstantDefinition
 import com.github.cubuspl42.sigmaLang.analyzer.semantics.introductions.VariableIntroduction
 
