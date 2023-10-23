588e51e60864eb5adbb3ca4a17a00c9ffb078185
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-10-23 16:46:49 +0200

    Nuke `AnnotatableIntroduction`

--- analyzer/src/main/kotlin/com/github/cubuspl42/sigmaLang/analyzer/semantics/introductions/AnnotatableIntroduction.kt
+++
@@ -1,7 +0,0 @@
-package com.github.cubuspl42.sigmaLang.analyzer.semantics.introductions
-
-import com.github.cubuspl42.sigmaLang.analyzer.semantics.types.Type
-
-interface AnnotatableIntroduction : Introduction {
-    val annotatedType: Type?
-}
