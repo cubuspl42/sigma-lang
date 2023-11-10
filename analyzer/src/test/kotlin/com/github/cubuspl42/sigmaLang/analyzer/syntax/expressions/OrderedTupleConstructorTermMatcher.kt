ed3f230d4bc2bac414bec4d1d4f42367c611daf3
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-11-10 13:14:37 +0100

    Implement complex "this type" method definition syntax

--- analyzer/src/test/kotlin/com/github/cubuspl42/sigmaLang/analyzer/syntax/expressions/OrderedTupleConstructorTermMatcher.kt
+++ analyzer/src/test/kotlin/com/github/cubuspl42/sigmaLang/analyzer/syntax/expressions/OrderedTupleConstructorTermMatcher.kt
@@ -1,10 +1,19 @@
 package com.github.cubuspl42.sigmaLang.analyzer.syntax.expressions
 
+import utils.ListMatchers
 import utils.Matcher
 
 class OrderedTupleConstructorTermMatcher(
     val elements: Matcher<List<ExpressionTerm>>,
 ) : Matcher<OrderedTupleConstructorTerm>() {
+    companion object {
+        fun withElementsInOrder(
+            vararg elements: Matcher<ExpressionTerm>,
+        ): Matcher<OrderedTupleConstructorTerm> = OrderedTupleConstructorTermMatcher(
+            elements = ListMatchers.inOrder(*elements),
+        )
+    }
+
     override fun match(actual: OrderedTupleConstructorTerm) {
         elements.match(actual = actual.elements)
     }
