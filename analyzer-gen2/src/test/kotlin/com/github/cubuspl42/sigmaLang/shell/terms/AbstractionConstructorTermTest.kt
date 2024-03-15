e69c88148d20b22da0a215c147461f909944f0b9
Jakub Trzebiatowski <cubuspl42@gmail.com>
2024-03-15 14:38:45 +0100

    Update the abstraction constructor term test

--- analyzer-gen2/src/test/kotlin/com/github/cubuspl42/sigmaLang/shell/terms/AbstractionConstructorTermTest.kt
+++ analyzer-gen2/src/test/kotlin/com/github/cubuspl42/sigmaLang/shell/terms/AbstractionConstructorTermTest.kt
@@ -1,17 +1,15 @@
 package com.github.cubuspl42.sigmaLang.shell.terms
 
+import com.github.cubuspl42.sigmaLang.core.values.StringValue
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class AbstractionValueConstructorTermTest {
+class AbstractionConstructorTermTest {
     @Test
-    fun testSimple() {
+    fun testNamedArgs() {
         val term = AbstractionConstructorTerm.parse(
             source = """
-                ^{arg3: Type1} => {
-                    a1 = {},
-                    a2 = arg3,
-                },
+                ^{arg3: Type1} => "result"
             """.trimIndent()
         )
 
@@ -27,19 +25,8 @@ class AbstractionValueConstructorTermTest {
                         ),
                     ),
                 ),
-                image = UnorderedTupleConstructorTerm(
-                    entries = listOf(
-                        UnorderedTupleConstructorTerm.Entry(
-                            key = IdentifierTerm("a1"),
-                            value = UnorderedTupleConstructorTerm.Empty,
-                        ),
-                        UnorderedTupleConstructorTerm.Entry(
-                            key = IdentifierTerm("a2"),
-                            value = ReferenceTerm(
-                                referredName = IdentifierTerm(name = "arg3"),
-                            ),
-                        ),
-                    ),
+                image = StringLiteralTerm(
+                    value = StringValue(value = "result"),
                 ),
             ),
             actual = term,
